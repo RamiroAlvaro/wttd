4c9d396c29fb207c37d761e59e065fdf872a5a20
Ramiro Alvaro <ramiroalvaro@hotmail.com>
2019-08-29 16:03:38 -0300

    Thank you page

---
+++ eventex/subscriptions/tests/test_view_detail.py
@@ -0,0 +1,39 @@
+from django.test import TestCase
+
+from eventex.subscriptions.models import Subscription
+
+
+class SubscriptionDetailGet(TestCase):
+    def setUp(self):
+        self.obj = Subscription.objects.create(
+            name='Ramiro Alvaro',
+            cpf='12345678901',
+            email='ramiroalvaro@hotmail.com',
+            phone='31-99138-7178'
+        )
+        self.response = self.client.get('/inscricao/{}/'.format(self.obj.pk))
+
+    def test_get(self):
+        self.assertEqual(200, self.response.status_code)
+
+    def test_template(self):
+        self.assertTemplateUsed(self.response,
+                                'subscriptions/subscription_detail.html')
+
+    def test_context(self):
+        subscription = self.response.context['subscription']
+        self.assertIsInstance(subscription, Subscription)
+
+    def test_html(self):
+        contents = (self.obj.name, self.obj.cpf,
+                    self.obj.email, self.obj.phone)
+        for expected in contents:
+            with self.subTest():
+                self.assertContains(self.response, expected)
+
+
+class SubscriptionDetailNotFound(TestCase):
+    def test_not_found(self):
+        response = self.client.get('/inscricao/0/')
+        self.assertEqual(404, response.status_code)
+
--- eventex/subscriptions/tests/test_view_subscribe.py
+++ eventex/subscriptions/tests/test_view_subscribe.py
@@ -44,8 +44,8 @@ class SubscribePostValid(TestCase):
         self.response = self.client.post('/inscricao/', data)
 
     def test_post(self):
-        """Valid POST should redirect to /inscricao/"""
-        self.assertEqual(302, self.response.status_code)
+        """Valid POST should redirect to /inscricao/1/"""
+        self.assertRedirects(self.response, '/inscricao/12/')
 
     def test_send_subscribe_email(self):
         self.assertEqual(1, len(mail.outbox))
@@ -78,11 +78,3 @@ class SubscribePostInvalid(TestCase):
     def test_dont_save_subscription(self):
         self.assertFalse(Subscription.objects.exists())
 
-
-class SubscribeSuccessMessage(TestCase):
-    def test_message(self):
-        data = dict(name='Ramiro Alvaro', cpf='12345678901',
-                    email='ramiroalvaro@hotmail.com', phone='31-99138-7178')
-        response = self.client.post('/inscricao/', data, follow=True)
-
-        self.assertContains(response, 'Inscrição realizada com sucesso!')
